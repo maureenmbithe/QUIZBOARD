a9a65d417ff78e859d95490099c5a996ce6d8ae2
maureenmbithe <112892541+maureenmbithe@users.noreply.github.com>
2023-01-18 17:51:44 +0300

    files edited

--- js/script.js
+++ js/script.js
@@ -1,50 +1,57 @@
-var result1 = 1;
-var result2 = 2;
-var result3 = 3;
-var result4 = 4;
-var result5 = 5;
-
-function score ()  {
-  return "you have scored" + total;
-}
-$(document).ready(function() {
-  $("#button2").click(function(event) {
-    var question1 = $("input:radio[name='question1']:checked").val();
-    var question2 = $("input:radio[name='question2']:checked").val();
-    var question3 = $("input:radio[name='question3']:checked").val();
-    var question4 = $("input:radio[name='question4']:checked").val();
-    var question5 = $("input:radio[name='question5']:checked").val();
-    
-    var correct = 0;
-    if (question1 === "right") {
-      result1 = 20;
-    }else {
-      result1 = 0;
-    }
-    if (question2 ==="right") {
-      result2 = 20;
-    } else {
-      result2 = 0;
-    }
-    if (question3 === "right") {
-      result3 = 20;
-    } else {
-      result3 = 0;
-    }
-    if (question4 === "right") {
-      result4 = 20;
-    } else {
-      result4 = 0;
-    }
-    if (question5 === "right") {
-      result5 = 20;
-    }else {
-      result5= 0;
-    }
-    var score = (result1 + result2 + result3 + result4 + result5);
-    console.log(score);
-
-    $("#result").text("You have gotten " + score + " points.");
-    
+//Business logic //
+$(document).ready(function () {
+    $("#btn1").click(function() {
+      var nameOne = $("input#name1").val();
+      var nameTwo = $("input#name2").val();
+  
+    })
   })
-})
+  
+  
+  
+  
+  //U.I Logic//
+  $(document).ready(function () {
+      $("#quiz").submit(function (event) {
+          event.preventDefault();
+          var quiz1= $("input:radio[name=Q1]:checked").val();
+          var quiz2 = $("input:radio[name=Q2]:checked").val();
+          var quiz3= $("input:radio[name=Q3]:checked").val();
+          var quiz4 = $("input:radio[name=Q4]:checked").val();
+          var quiz5= $("input:radio[name=Q5]:checked").val();
+  
+  
+  
+          var correct = 0;
+          if (quiz1=="Style"){
+            correct++;
+          }
+          if (quiz2=="Single quotes"){
+            correct++;
+          }
+          if (quiz3=="scripting"){
+            correct++;
+          }
+          if (quiz4=="To add interactivity on HTML"){
+            correct++;
+          }
+          if (quiz5=="semi-colon"){
+            correct++;
+          }
+          var percent = correct/5*100
+          if (percent >69){
+            remark="EXELLENT!"
+          }
+          if(percent >49){
+            remark="FAIR!"
+  
+          }
+          else {
+            remark="Are you really a student here?!!"
+          };
+  
+        document.getElementById("correct").innerHTML = "You scored " + correct +" out of 5 questions correct."
+      document .getElementById("results").innerHTML = remark;
+      });
+  
+  });
