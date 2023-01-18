ca692d22e6d7f7c956ec3fda58ca9d4cdb51fc39
maureenmbithe <112892541+maureenmbithe@users.noreply.github.com>
2023-01-18 16:49:04 +0300

    files amended

--- js/script.js
+++ js/script.js
@@ -4,22 +4,18 @@ var result3 = 3;
 var result4 = 4;
 var result5 = 5;
 
-function score () {
+function score ()  {
   return "you have scored" + total;
 }
 $(document).ready(function() {
-  $("#button2").click(function() {
-    var question1 = $("input[name='question1']:checked").val();
-    var question2 = $("input[name='question2']:checked").val();
-    var question3 = $("input[name='question3']:checked").val();
-    var question4 = $("input[name='question4']:checked").val();
-    var question5 = $("input[name='question5']:checked").val();
-    console.log(question1);
-    console.log(question2);
-    console.log(question3);
-    console.log(question4);
-    console.log(question5);
-
+  $("#button2").click(function(event) {
+    var question1 = $("input:radio[name='question1']:checked").val();
+    var question2 = $("input:radio[name='question2']:checked").val();
+    var question3 = $("input:radio[name='question3']:checked").val();
+    var question4 = $("input:radio[name='question4']:checked").val();
+    var question5 = $("input:radio[name='question5']:checked").val();
+    
+    var correct = 0;
     if (question1 === "right") {
       result1 = 20;
     }else {
